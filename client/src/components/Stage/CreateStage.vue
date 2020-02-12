6edcfdc5ec6642c3d62231246433b89263ce55aa
JKlerk <99045878@mydavinci.nl>
2020-02-12 17:18:53 +0100

    Create/Edit stage

---
+++ client/src/components/Stage/CreateStage.vue
@@ -0,0 +1,51 @@
+<template>
+    <transition name="fade" v-if="visible">
+        <div>
+            <div class="bg-black opacity-75 fixed min-h-screen z-0 w-full top-0"></div>
+            <div class="flex justify-center">
+                <div class="z-40 bg-white w-1/3 fixed rounded-lg shadow overflow-auto top-0 mt-5" style="max-height: 600px">
+                    <div class="shadow flex flex-col items-center justify-center p-10 rounded-t-lg select-none relative" style="height: 350px; background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(54, 13, 123, 0.8)), url(https://images.unsplash.com/photo-1464375117522-1311d6a5b81f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=2250&amp;q=80);background-repeat: no-repeat;background-position: center center;">
+                        <h1 class="font-bold text-3xl text-white">{{ stage.name }}</h1>
+                    </div>
+                    <div class="p-6">
+                        <div class="mx-6">
+                            <div class="mb-6">
+                                <div class="py-4 mt-5">
+                                    <div class="mb-4">
+                                        <label class="mr-2 font-medium text-sm tracking-wide text-purple-800 mt-4">Stage name:</label>
+                                        <input v-model="stage.name" class="w-full block mt-2 rounded-lg border border-purple-200 focus:outline-none appearance-none px-2 py-2 leading-3 text-sm">
+                                    </div>
+                                    <div class="mb-4">
+                                        <label class="mr-2 font-medium text-sm tracking-wide text-purple-800 mt-4">Stage Description:</label>
+                                        <textarea rows="5" v-model="stage.desc" class="w-full block mt-2 rounded-lg border border-purple-200 focus:outline-none appearance-none px-2 py-2 leading-4 text-sm"></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex mt-5 items-center justify-between bg-purple-100 border-t-2 border-purple-200 px-8 py-6 -m-6 rounded-b-lg">
+                            <button class="text-white bg-purple-500 hover:bg-purple-600 rounded-full px-8 py-2 text-sm font-semibold transition duration-100 focus:outline-none">Add</button>
+                            <button @click="visible = false, stage = { name: 'Please enter a name'}" class="text-purple-100 bg-purple-700 hover:bg-purple-800 rounded-full px-8 py-2 text-sm font-semibold transition duration-100 focus:outline-none">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+import { Edit2Icon } from 'vue-feather-icons'
+export default {
+    data(){
+        return{
+            visible: false,
+            stage: {
+                name: 'Please enter a name'
+            },
+        }
+    },
+    components:{
+        Edit2Icon,
+    },
+}
+</script>
--- client/src/views/Stages.vue
+++ client/src/views/Stages.vue
@@ -1,41 +1,49 @@
 <template>
-	<div class="flex justify-center pt-5 pb-10">
-		<div class="mx-20 w-2/3">
-			<!-- Header -->
-			<div class="flex">
-				<h1 class="text-3xl font-bold">Stages</h1>
-				<div class="flex flex-1 justify-end">
-					<div class="w-1/2 text-center bg-white block mt-2 rounded-lg border border-purple-200 focus:outline-none appearance-none px-2 py-2 leading-2 text-sm mx-2 flex">
-						<div class="my-auto mr-3">
-							<search-icon size="1.0x" />
-						</div>
-						<input placeholder="Search for stages" class="appearance-none focus:outline-none w-full">
-					</div>
-				</div>
-			</div>
-			<!-- Body -->
-			<div class="flex flex-wrap mt-10">
-				<div class="flex shadow rounded-lg bg-white w-1/2 hover:opacity-50 transition duration-200 cursor-pointer mb-5" :key="stage.id" v-for="stage in getStages()">
-					<div class="rounded-l-lg w-1/3" style="background-image: url('https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'); background-position: center; background-size: cover;"></div>
-					<div class="py-10 w-2/3">
-						<h2 class="text-2xl ml-4 text-purple-700">{{ stage.name }}</h2>
-						<p class="text-gray-700 ml-4">{{ stage.desc }}</p>
-					</div>
-				</div>
-				<div class="bg-white w-1/2 rounded-lg shadow h-auto flex items-center justify-center hover:bg-gray-100 cursor-pointer">
-					<p class="text-6xl text-purple-600">+</p>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div>
+        <CreateStage ref="createstage" />
+		<EditStage ref="editstage" />
+        <div class="flex justify-center pt-5 pb-10">
+            <div class="mx-20 w-2/3">
+                <!-- Header -->
+                <div class="flex">
+                    <h1 class="text-3xl font-bold">Stages</h1>
+                    <div class="flex flex-1 justify-end">
+                        <div class="w-1/2 text-center bg-white block mt-2 rounded-lg border border-purple-200 focus:outline-none appearance-none px-2 py-2 leading-2 text-sm mx-2 flex">
+                            <div class="my-auto mr-3">
+                                <search-icon size="1.0x" />
+                            </div>
+                            <input placeholder="Search for stages" class="appearance-none focus:outline-none w-full">
+                        </div>
+                    </div>
+                </div>
+                <!-- Body -->
+                <div class="flex flex-wrap mt-10">
+                    <div class="flex shadow rounded-lg bg-white w-1/2 hover:opacity-50 transition duration-200 cursor-pointer mb-5" @click="$refs.editstage.visible = true, $refs.editstage.stage = stage" :key="stage.id" v-for="stage in getStages()">
+                        <div class="rounded-l-lg w-1/3" style="background-image: url('https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'); background-position: center; background-size: cover;"></div>
+                        <div class="py-10 w-2/3">
+                            <h2 class="text-2xl ml-4 text-purple-700">{{ stage.name }}</h2>
+                            <p class="text-gray-700 ml-4">{{ stage.desc }}</p>
+                        </div>
+                    </div>
+                    <div @click="$refs.createstage.visible = true" class="bg-white w-1/2 rounded-lg shadow h-auto flex items-center justify-center hover:bg-gray-100 cursor-pointer">
+                        <p class="text-6xl text-purple-600">+</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
 import { SearchIcon } from 'vue-feather-icons'
+import CreateStage from '@/components/Stage/CreateStage'
+import EditStage from '@/components/Stage/EditStage'
 import stagesData from '@/stages.json'
 
 export default {
 	components:{
 		SearchIcon,
+        CreateStage,
+        EditStage,
 	},
 	methods:{
 		getStages(){
