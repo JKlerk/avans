ea2fc1e996105e237cdc4bc17a4e30b665f468ab
JKlerk <99045878@mydavinci.nl>
2020-02-13 17:19:35 +0100

    Added stagecontroller

--- src/main/java/nl/jklerk/school/Controllers/StageController.java
+++ src/main/java/nl/jklerk/school/Controllers/StageController.java
@@ -3,12 +3,12 @@ package nl.jklerk.school.Controllers;
 import nl.jklerk.school.Database;
 import nl.jklerk.school.Response;
 
-public class ArtistController {
-
+public class StageController {
+    //Setup with database
     private static final Database database = new Database();
 
-    public static Response getArtists() {
+    public static Response getStages() {
         Response response = new Response();
-        return response.withBody(database.query("SELECT * FROM artists"));
+        return response.withBody(database.query("SELECT * FROM stages"));
     }
 }
--- src/main/java/nl/jklerk/school/Main.java
+++ src/main/java/nl/jklerk/school/Main.java
@@ -1,5 +1,8 @@
 package nl.jklerk.school;
 
+import nl.jklerk.school.Controllers.ArtistController;
+import nl.jklerk.school.Controllers.StageController;
+
 import java.io.IOException;
 
 public class Main {
@@ -7,6 +10,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         final var server = new Server(8001);
         server.addEndpoint("/api/artists", Server.RequestMethod.GET, ArtistController::getArtists);
+        server.addEndpoint("/api/stages", Server.RequestMethod.GET, StageController::getStages);
         server.start();
     }
 }
